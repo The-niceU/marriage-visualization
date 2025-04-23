9ce675ccc38f4e8f4ca685cf2300dfeb85b7d201
llj123456789 <2041584846@qq.com>
2025-04-23 22:43:14 +0800

    version : 0.0.1

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<script setup>
+import MV from './components/MarriageVisual.vue';
+
+</script>
+
+<template>
+  <MV data_path="/src/assets/data/marriage.csv"/>
+</template>
+
+<style scoped>
+
+</style>
